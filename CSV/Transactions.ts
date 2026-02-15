f86656c45e3138f583a25dc59a0452d5bbe0c3e9
nemagemo <krzysiekjach@gmail.com>
2026-02-15 18:25:38 +0100

    feat: Add new crypto and IKE transactions
    Adds new entries to the `Transactions.ts` and `OMFopen.ts` CSV files. These entries represent recent cryptocurrency purchases and new stock acquisitions within the IKE investment account.

--- CSV/Transactions.ts
+++ CSV/Transactions.ts
@@ -5,4 +5,7 @@ export const TRANSACTIONS_DATA = `Data,Portfel,Typ,Symbol,Ilość,Koszt,Waluta
 2026-02-03,Krypto,Sprzedaż,USDC,"50,000000","178,50 zł",PLN
 2026-02-03,Krypto,Kupno,BTC,"0,00066780","178,50 zł",PLN
 2026-02-05,Krypto,Sprzedaż,USDC,"50,000000","178,50 zł",PLN
-2026-02-05,Krypto,Kupno,BTC,"0,00075180","178,50 zł",PLN`;
+2026-02-05,Krypto,Kupno,BTC,"0,00075180","178,50 zł",PLN
+2026-02-13,IKE,Kupno,AMZN,"0,070000","49,94 zł",PLN
+2026-02-13,IKE,Kupno,MSFT,"0,035000","49,97 zł",PLN
+2026-02-13,IKE,Kupno,ROL,"0,382100","79,68 zł",PLN`;
