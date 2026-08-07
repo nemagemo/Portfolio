4b15ba01086eaa1e2ad3d0fe510e85c7d0923700
nemagemo <krzysiekjach@gmail.com>
2026-08-07 14:28:18 +0200

    chore: update IKE portfolio and transactions
    Update asset data for FRO and PLN-IKE, and record the recent FRO purchase in the transaction log.

--- CSV/Transactions.ts
+++ CSV/Transactions.ts
@@ -89,4 +89,5 @@ export const TRANSACTIONS_DATA = `Data,Portfel,Typ,Symbol,Ilość,Koszt,Waluta,S
 2026-08-07,IKE,Kupno,PLW,"0,200800","49,90 zł",PLN,
 2026-08-07,IKE,Kupno,KLE,"8,000000","57,60 zł",PLN,
 2026-08-07,IKE,Kupno,IUIT.L,"0,583100","112,31 zł",PLN,
-2026-08-07,IKE,Kupno,GAW,"0,052100","49,88 zł",PLN,`;
+2026-08-07,IKE,Kupno,GAW,"0,052100","49,88 zł",PLN,
+2026-08-07,IKE,Kupno,FRO,"1,474900","50,00 zł",PLN,`;
