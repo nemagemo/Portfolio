00c78df775bf509b13842d561f2a6b0f963fadbb
nemagemo <krzysiekjach@gmail.com>
2026-08-07 12:19:59 +0200

    chore: update portfolio data and transactions
    Sync OMFopen positions and add recent IUIT.L transaction record.

--- CSV/OMFopen.ts
+++ CSV/OMFopen.ts
@@ -1,7 +1,7 @@
 export const OMF_OPEN_DATA = `Status pozycji,Portfel,Typ,Symbol,ISIN,Sektor,Okres inwestycji,Ostatni zakup,Ilość,Obecna wartość,Wartość zakupu,Zysk/Strata,ROI
 Otwarta,PPK,PPK,PPK,,,,"2026-08-03","95,1878","11 502,44 zł","3 926,02 zł","7 576,42 zł","192,98%"
 Otwarta,IKE,ETF,NDIA.L,IE00BZCQB185,,416,2026-08-07,"45,893700","1 532,04 zł","1 709,09 zł","-177,05 zł","-10,36%"
-Otwarta,IKE,ETF,IUIT.L,IE00B3WJKG14,"Information Technology",416,2026-07-22,"12,002900","2 144,75 zł","1 596,91 zł","547,84 zł","34,31%"
+Otwarta,IKE,ETF,IUIT.L,IE00B3WJKG14,"Information Technology",416,2026-08-07,"12,586000","2 257,06 zł","1 709,22 zł","547,84 zł","32,05%"
 Otwarta,IKE,ETF,NUKL.DE,IE000M7V94E1,,382,2026-07-22,"9,995800","1 849,23 zł","1 406,40 zł","442,83 zł","31,49%"
 Otwarta,IKE,ETF,QUTM.DE,IE0007Y8Y157,"Information Technology",0,2026-07-22,"7,697500","799,54 zł","812,17 zł","-12,63 zł","-1,56%"
 Otwarta,IKE,Akcje,AMZN,US0231351067,"Consumer Discretionary",408,2026-07-02,"1,516600","1 536,15 zł","1 197,16 zł","338,99 zł","28,32%"
@@ -20,7 +20,7 @@ Otwarta,IKE,Akcje,FAST,US3119001044,Industrials,226,2025-05-06,"2,303200","409,8
 Otwarta,IKE,Akcje,GAW,GB0003718474,"Consumer Discretionary",412,2026-07-22,"1,542700","1 483,49 zł","1 065,43 zł","418,06 zł","39,24%"
 Otwarta,IKE,Akcje,ROL,US7757111049,Industrials,412,2026-02-13,"4,203200","595,23 zł","855,77 zł","-260,54 zł","-30,45%"
 Otwarta,IKE,Akcje,FRO,PLFERRO00016,Industrials,286,2026-07-22,"23,031900","766,96 zł","814,00 zł","-47,04 zł","-5,78%"
-Otwarta,IKE,Gotówka,PLN-IKE,,,0,2026-08-07,"0,000000","1 181,77 zł","1 181,77 zł","0,00 zł","0,00%"
+Otwarta,IKE,Gotówka,PLN-IKE,,,0,2026-08-07,"0,000000","1 069,46 zł","1 069,46 zł","0,00 zł","0,00%"
 Otwarta,Krypto,Krypto,POL,,,719,2024-10-04,"587,863288","159,72 zł","1 033,13 zł","-873,41 zł","-84,54%"
 Otwarta,Krypto,Krypto,ETH,,,997,2024-10-04,"0,098388","689,19 zł","956,50 zł","-267,31 zł","-27,95%"
 Otwarta,Krypto,Krypto,NEAR,,,934,2026-06-06,"59,619531","378,01 zł","652,06 zł","-274,05 zł","-42,03%"
